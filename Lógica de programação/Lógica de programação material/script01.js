305f7e8fc36ca5471befbf2344a5c3208ce63b05
Guilherme <100242616+GuiDEV1@users.noreply.github.com>
2022-08-24 12:45:47 -0300

    Add files via upload

---
+++ Lógica de programação/Lógica de programação material/script01.js
@@ -0,0 +1,11 @@
+
+var hora_de_ir_ao_medico , preco, mensagem, feliz, porta_aberta;
+
+    hora_de_ir_ao_medico = 9;
+    preco = 7.50;
+    mensagem = 'Ola mundo!!';
+    feliz = true;
+    porta_aberta = true;
+
+    alert("A hora ir ao médico é: " + hora_de_ir_ao_medico + ' h'); 
+
