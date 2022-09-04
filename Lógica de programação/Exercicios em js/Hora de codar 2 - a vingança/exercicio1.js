363c7fb7985e34a82be24355da5d7cd87d4a5075
Guilherme <100242616+GuiDEV1@users.noreply.github.com>
2022-09-04 16:43:28 -0300

    Update exercicio1.js

--- Lógica de programação/Exercicios em js/Hora de codar 2 - a vingança/exercicio1.js
+++ Lógica de programação/Exercicios em js/Hora de codar 2 - a vingança/exercicio1.js
@@ -2,12 +2,12 @@
  Programa que o usuário informa dois numeros , e retrona o maior numero
  Data: 26/08/2022
  Autor: Guilherme M. Dias
- Versão: 1.0
+ Versão: 1.1.0
 */
 var n1, n2;
 
 n1 = parseInt(prompt("Informe o primeiro valor: "));
-n2 = parseInt(prompt("Informe o primeiro valor: "));
+n2 = parseInt(prompt("Informe o segundo valor: "));
 
 if (n1 === n2) {
     document.write('Eles são iguais!');
